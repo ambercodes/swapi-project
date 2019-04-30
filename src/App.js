9937bdbe73f4935b6e3c83949f9b91e0f2ff3534
Amber Codes <agalvanosi@gmail.com>
2019-04-29 22:42:42 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }from 'react';
+import axios from 'axios';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      people: [
+        {
+          id: 1,
+          name: '',
+          birth_year: '',
+          gender: '',
+          mass: '',
+          height: ''
+        }],
+      columns: [
+          {
+          dataField: 'id',
+          text: 'ID'
+        },
+        {
+          dataField: 'name',
+          text: 'Name'
+        },
+        {
+          dataField: 'birth_year',
+          text: 'Birth Year'
+        },
+        {
+          dataField: 'gender',
+          text: 'Gender'
+        },
+        {
+          dataField: 'mass',
+          text: 'Mass'
+        },
+        {
+          dataField: 'height',
+          text: 'Height'
+        }],
+      isLoading: true,
+      errors: null
+    }
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios.get('https://swapi.co/api/people/1').then(response => console.log(response))
+    .catch(error => this.setState({ error, isLoading: false }));
+  }
+
+  render() {
+    return (
+      <div className= "container"  >
+        <BootstrapTable
+         data= { this.state.people }
+         columns= { this.state.columns }
+         keyField= 'id' />
+      </div>
+    );
+  }
 }
 
 export default App;
